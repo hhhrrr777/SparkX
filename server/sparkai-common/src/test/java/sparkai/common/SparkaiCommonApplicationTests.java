9526f0ae0f885e2b92781fbb13dbd05d91ca78b3
SparkShop <564868623@qq.com>
2025-02-18 22:28:36 +0800

    基础架构

---
+++ server/sparkai-common/src/test/java/sparkai/common/SparkaiCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package sparkai.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkaiCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ server/sparkai-web/src/main/java/sparkai/sparkaiweb/SparkaiWebApplication.java
@@ -0,0 +1,13 @@
+package sparkai.sparkaiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkaiWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkaiWebApplication.class, args);
+    }
+
+}
