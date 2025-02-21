af33c8c5b2a049ae7163016de2f4f538888185c4
NickBai <876337011@qq.com>
2025-02-21 14:54:04 +0800

    添加版权

--- server/sparkai-service/src/main/java/sparkai/service/service/impl/system/UserServiceImpl.java
+++ server/sparkai-service/src/main/java/sparkai/service/service/impl/system/UserServiceImpl.java
@@ -1,8 +1,16 @@
+// +----------------------------------------------------------------------
+// | SparkAI 基于大语言模型和 RAG 的知识库问答系统
+// +----------------------------------------------------------------------
+// | Copyright (c) 2022~2099 http://sparkai.sparkshop.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed SparkAI 并不是自由软件，未经许可不能去掉 SparkAI 相关版权
+// +----------------------------------------------------------------------
+// | Author: NickBai  <1902822973@qq.com>
+// +----------------------------------------------------------------------
 package sparkai.service.service.impl.system;
 
 import cn.hutool.core.lang.ObjectId;
 import cn.hutool.core.util.IdUtil;
-import cn.hutool.core.util.ObjectUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
--- server/sparkai-service/src/main/java/sparkai/service/vo/system/UserVo.java
+++ server/sparkai-service/src/main/java/sparkai/service/vo/system/UserVo.java
@@ -1,4 +1,31 @@
+// +----------------------------------------------------------------------
+// | SparkAI 基于大语言模型和 RAG 的知识库问答系统
+// +----------------------------------------------------------------------
+// | Copyright (c) 2022~2099 http://sparkai.sparkshop.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed SparkAI 并不是自由软件，未经许可不能去掉 SparkAI 相关版权
+// +----------------------------------------------------------------------
+// | Author: NickBai  <1902822973@qq.com>
+// +----------------------------------------------------------------------
 package sparkai.service.vo.system;
 
+import lombok.Data;
+
+@Data
 public class UserVo {
+
+    /**
+     * 用户名
+     */
+    private String name;
+
+    /**
+     * 用户状态
+     */
+    private Integer status;
+
+
+    private long page;
+
+    private long limit;
 }
