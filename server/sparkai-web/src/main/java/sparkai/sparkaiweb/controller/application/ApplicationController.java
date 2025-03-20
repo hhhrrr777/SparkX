fbbaa1927b78ae29d58460ce59559c6ec6664199
NickBai <876337011@qq.com>
2025-03-20 17:00:03 +0800

    组件封装

---
+++ server/sparkai-web/src/main/java/sparkai/sparkaiweb/controller/application/ApplicationController.java
@@ -0,0 +1,11 @@
+package sparkai.sparkaiweb.controller.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/application")
+@RestController
+public class ApplicationController {
+
+}
