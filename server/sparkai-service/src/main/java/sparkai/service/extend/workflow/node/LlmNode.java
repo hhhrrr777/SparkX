d763589a74c8dbed71ab7ce7ff1475fdef249342
NickBai <876337011@qq.com>
2025-05-15 16:52:33 +0800

    知识库检索完成

--- server/sparkai-service/src/main/java/sparkai/service/extend/workflow/node/LlmNode.java
+++ server/sparkai-service/src/main/java/sparkai/service/extend/workflow/node/LlmNode.java
@@ -2,9 +2,13 @@ package sparkai.service.extend.workflow.node;
 
 import cn.hutool.json.JSONObject;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import sparkai.service.extend.workflow.IWorkflowNode;
+import sparkai.service.helper.ChatModelBuildHelper;
+import sparkai.service.mapper.application.ApplicationWorkflowRuntimeContextMapper;
+import sparkai.service.mapper.system.ModelsMapper;
 import sparkai.service.vo.workflow.EdgeVo;
 import sparkai.service.vo.workflow.NodeVo;
 
@@ -17,8 +21,20 @@ public class LlmNode implements IWorkflowNode {
     @Setter
     public SseEmitter emitter;
 
+    @Autowired
+    ApplicationWorkflowRuntimeContextMapper applicationWorkflowRuntimeContextMapper;
+
+    @Autowired
+    ModelsMapper modelsMapper;
+
+    @Autowired
+    ChatModelBuildHelper chatModelBuildHelper;
+
     @Override
     public List<EdgeVo> handle(NodeVo nodeInfo, long runtimeId, String sourceId, Map<String, List<EdgeVo>> edges) {
+
+        JSONObject nodeObject = nodeInfo.getData();
+
         return null;
     }
 }
