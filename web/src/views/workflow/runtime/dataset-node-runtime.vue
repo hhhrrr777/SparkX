2bc30a61287e97d2f9946d96beea664133eeb2c7
NickBai <876337011@qq.com>
2025-06-16 17:08:17 +0800

    展示不同的上下文

--- web/src/views/workflow/runtime/dataset-node-runtime.vue
+++ web/src/views/workflow/runtime/dataset-node-runtime.vue
@@ -3,7 +3,7 @@
 		<div class="detail-title">检索内容</div>
 		<div class="detail-content">
 			<div class="content-item">
-				欢迎还用
+				{{ runtimeData.outputData['sys.question'] }}
 			</div>
 		</div>
 	</div>
